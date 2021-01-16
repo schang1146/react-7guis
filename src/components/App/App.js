4eabfc794046ab75c326c5517166b0fbc75a363a
Sammy Chang <sammychang2185@gmail.com>
2021-01-16 11:30:40 -0800

    Add files and imports for TempConverter (no functionality)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import Counter from '../Counter/Counter';
+import TempConverter from '../TempConverter/TempConverter';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className='App'>
       <h1>7Guis: A GUI Programming Benchmark</h1>
       <Counter />
+      <TempConverter />
     </div>
   );
 }
---
+++ src/components/TempConverter/TempConverter.js
@@ -0,0 +1,9 @@
+function TempConverter() {
+  return (
+    <div>
+      <h2>Temperature Converter</h2>
+    </div>
+  );
+}
+
+export default TempConverter;

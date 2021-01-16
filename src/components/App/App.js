86313a45b9d6fff31949082cc9d827a93ef13306
Sammy Chang <sammychang2185@gmail.com>
2021-01-16 11:36:26 -0800

    Add files and imports for Timer (no functionality)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import Counter from '../Counter/Counter';
+import Timer from '../Timer/Timer';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className='App'>
       <h1>7Guis: A GUI Programming Benchmark</h1>
       <Counter />
+      <Timer />
     </div>
   );
 }

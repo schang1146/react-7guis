2142b3be9bf71068335a24f2325776822f3357f8
Sammy Chang <sammychang2185@gmail.com>
2021-01-16 11:23:32 -0800

    Add Counter to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
+import Counter from '../Counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <h1>7Guis: A GUI Programming Benchmark</h1>
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <input value={counter} />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Count
+      </button>
+    </div>
+  );
+}
+
+export default Counter;

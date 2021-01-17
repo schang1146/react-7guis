30b93d5394bd4874cd516e0c1a3b8bf04cadcea9
Sammy Chang <sammychang2185@gmail.com>
2021-01-17 01:10:32 -0800

    Add basic layout and functionality to select flight type

--- src/components/FlightBooker/FlightBooker.js
+++ src/components/FlightBooker/FlightBooker.js
@@ -1,7 +1,23 @@
+import React, { useState } from 'react';
+
 function FlightBooker() {
+  const [flightType, setFlightType] = useState('oneWay');
+  const [departure, setDeparture] = useState('27.03.2014');
+  const [arrival, setArrival] = useState('27.03.2014');
+
   return (
     <div>
       <h2>Flight Booker</h2>
+      <select name='flightType' onChange={(e) => setFlightType(e.target.value)}>
+        <option value='oneWay'>one-way flight</option>
+        <option value='return'>return flight</option>
+      </select>
+      <br />
+      <input value={departure} />
+      <br />
+      <input value={arrival} disabled={flightType === 'oneWay' ? true : false} />
+      <br />
+      <button>Book</button>
     </div>
   );
 }

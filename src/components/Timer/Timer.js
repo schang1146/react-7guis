0af8a4636509b20c0c187c02de961761d08788d2
Sammy Chang <sammychang2185@gmail.com>
2021-01-18 01:16:02 -0800

    Scaffold Timer component that starts on mount

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,7 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+function ProgressBar() {
+  return <div>Progress Bar!</div>;
+}
+
 function Timer() {
+  const [startTime, setStartTime] = useState(Date.now());
+  const [currentTime, setCurrentTime] = useState(null);
+  const [intervalId, setIntervalId] = useState(null);
+  const [timer, setTimer] = useState(20);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentTime(Date.now());
+    }, 100);
+  }, []);
+
   return (
     <div>
       <h2>Timer</h2>
+      Elapsed Time: <ProgressBar />
+      <br />
+      {`${Math.round((currentTime - startTime) / 100) / 10}`}s
+      <br />
+      Duration{' '}
+      <input
+        type='range'
+        min='5'
+        max='60'
+        value={timer}
+        onChange={(e) => {
+          setTimer(e.target.value);
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          setStartTime(Date.now());
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }

eeb4578260178c562714bb74cefa15d14a9752be
Sammy Chang <sammychang2185@gmail.com>
2021-04-26 11:34:02 -0700

    Saving old changes

--- src/components/Cells/Cells.js
+++ src/components/Cells/Cells.js
@@ -15,12 +15,37 @@ function TableHeader({ width }) {
 }
 
 function TableBody({ width, height }) {
+  const [active, setActive] = useState(null);
+
+  const initState = () => {
+    let table = [];
+    for (let i = 0; i < height; i++) {
+      let row = [];
+      for (let j = 0; j < width; j++) {
+        row.push('');
+      }
+      table.push(row);
+    }
+    return table;
+  };
+
+  // const [state, setState] = useState(initState());
+  let state = initState();
+  const setState = (id, value) => {
+    let [rowId, colId] = id.split(',');
+    state[parseInt(rowId)][parseInt(colId)] = value;
+  };
+  const getState = (id) => {
+    let [rowId, colId] = id.split(',');
+    return state[parseInt(rowId)][parseInt(colId)];
+  };
+
   let tableBody = [];
   for (let rowId = 0; rowId < height; rowId++) {
     let row = [];
     row.push(<td>{rowId}</td>);
     for (let colId = 0; colId < width; colId++) {
-      row.push(<Cell />);
+      row.push(<Cell id={`${rowId},${colId}`} isActive={active === `${rowId},${colId}` ? true : false} setActive={setActive} setState={setState} getState={getState} />);
     }
     tableBody.push(row);
   }
@@ -33,10 +58,73 @@ function TableBody({ width, height }) {
   );
 }
 
-function Cell() {
+function Cell({ id, isActive, setActive, setState, getState }) {
+  const [value, setValue] = useState('');
+
+  const handlerTextChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const parseCell = (data, seenIds = new Set()) => {
+    if (seenIds.has(id)) {
+      return '#REF!';
+    }
+    if (data[0] === '=') {
+      let output = '';
+      let cp = 1;
+      while (cp < data.length) {
+        if (data[cp].match(/[a-zA-Z]/)) {
+          let colRef = data[cp].charCodeAt(0) - 65;
+          let rowRef = '';
+          if (cp + 1 < data.length && data[cp + 1].match(/[0-9]/)) {
+            rowRef += data[cp + 1];
+            cp++;
+          }
+          if (cp + 1 < data.length && data[cp + 1].match(/[0-9]/)) {
+            rowRef += data[cp + 1];
+            cp++;
+          }
+          if (rowRef === '') {
+            return '#ERROR!';
+          } else {
+            seenIds.add(id);
+            let parsedCell = parseCell(getState(`${rowRef},${colRef}`));
+            if (parsedCell === '#REF!') {
+              return '#REF!';
+            } else {
+              console.log(parsedCell);
+              output += parsedCell;
+            }
+          }
+        } else if (data[cp].match(/[0-9-+/*()]/)) {
+          output += data[cp];
+        }
+        cp++;
+      }
+      setState(output);
+      return evaluate(output);
+    } else {
+      return data;
+    }
+  };
+
   return (
     <td>
-      <input />
+      <input
+        value={isActive ? value : parseCell(value)}
+        onChange={handlerTextChange}
+        onFocus={() => {
+          setActive(id);
+        }}
+        onBlur={() => {
+          setActive(null);
+        }}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            e.target.blur();
+          }
+        }}
+      />
     </td>
   );
 }
---
+++ src/components/Cells/Cells_old.js
@@ -0,0 +1,121 @@
+import { useEffect, useState } from 'react';
+import _ from 'lodash';
+import { evaluate } from 'mathjs';
+
+function TableHeader({ width }) {
+  let header = [<th key='top-left'>{/* Empty for top left corner */}</th>];
+  for (let charCode = 65; charCode < 65 + width; charCode++) {
+    header.push(<th key={String.fromCharCode(charCode)}>{String.fromCharCode(charCode)}</th>);
+  }
+  return <tr>{header}</tr>;
+}
+
+function Cells() {
+  const [activeElement, setActiveElement] = useState(null);
+  const [tableWidth, setTableWidth] = useState(3);
+  const [tableHeight, setTableHeight] = useState(10);
+
+  const initTable = () => {
+    let table = [];
+    for (let i = 0; i < tableHeight; i++) {
+      let row = [];
+      for (let j = 0; j < tableWidth; j++) {
+        row.push('');
+      }
+      table.push(row);
+    }
+    return table;
+  };
+
+  const [tableState, setTableState] = useState(initTable());
+
+  const handlerTextChange = (e) => {
+    const [row, col] = e.target.id.split(',');
+    let tableStateCopy = _.cloneDeep(tableState);
+    tableStateCopy[parseInt(row)][parseInt(col)] = e.target.value;
+    setTableState(tableStateCopy);
+  };
+
+  const parseCell = (data) => {
+    if (data[0] === '=') {
+      let output = '';
+      let cp = 1;
+      while (cp < data.length) {
+        if (data[cp].match(/[a-zA-Z]/)) {
+          let colRef, rowRef;
+          colRef = data[cp].charCodeAt(0) - 65;
+          if (cp + 1 < data.length && data[cp + 1].match(/[0-9]/)) {
+            if (cp + 2 < data.length && data[cp + 2].match(/[0-9]/)) {
+              rowRef = data[cp + 1] + data[cp + 2];
+              cp += 2;
+            } else {
+              rowRef = data[cp + 1];
+              cp++;
+            }
+          } else {
+            console.log('Invalid expression!');
+          }
+          if (rowRef !== undefined && colRef !== undefined && tableState[rowRef][colRef] !== '') {
+            output += parseCell(tableState[rowRef][colRef]);
+          } else {
+            return '';
+          }
+        } else if (data[cp].match(/[0-9-+/*()]/)) {
+          output += data[cp];
+        }
+        cp++;
+      }
+      console.log(output);
+      return evaluate(output);
+    } else {
+      return data;
+    }
+  };
+
+  const setCell = (e) => {
+    setActiveElement(null);
+    console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Cells</h2>
+      <table style={{ margin: '0 auto' }}>
+        <thead>
+          <TableHeader width={tableWidth} />
+        </thead>
+        <tbody>
+          {tableState.map((row, rowId) => {
+            return (
+              <tr key={`row-${rowId}`}>
+                <td>{rowId}</td>
+                {row.map((data, colId) => {
+                  return (
+                    <td key={`${rowId},${colId}`}>
+                      <input
+                        id={`${rowId},${colId}`}
+                        value={activeElement === `${rowId},${colId}` ? data : parseCell(data)}
+                        onChange={(e) => handlerTextChange(e)}
+                        onBlur={(e) => setCell(e)}
+                        onFocus={(e) => {
+                          setActiveElement(e.target.id);
+                        }}
+                        onKeyDown={(e) => {
+                          if (e.key === 'Enter') {
+                            e.target.blur();
+                          }
+                        }}
+                      />
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Cells;
